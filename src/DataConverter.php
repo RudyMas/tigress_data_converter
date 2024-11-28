7455ca171975fc1e1075befe48857e6e7f533505
RudyMasGoNext <rudy.mas@go-next.be>
2024-11-28 15:09:11 +0100

    Refactor to PHP 8.4

--- src/DataConverter.php
+++ src/DataConverter.php
@@ -6,13 +6,12 @@ use SimpleXMLElement;
 use stdClass;
 
 /**
- * Class Data Converter (PHP version 8.3)
+ * Class Data Converter (PHP version 8.4)
  *
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024, rudymas.be. (http://www.rudymas.be/)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 1.0.0
- * @lastmodified 2024-07-04
+ * @version 2024.11.28.0
  * @package Tigress\DataConverter
  */
 class DataConverter
@@ -30,9 +29,12 @@ class DataConverter
      */
     public static function version(): string
     {
-        return '1.0.0';
+        return '2024.11.28';
     }
 
+    /**
+     * DataConverter destructor
+     */
     public function __destruct()
     {
         unset($this->arrayData);
