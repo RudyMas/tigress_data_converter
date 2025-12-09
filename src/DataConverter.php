2cb645a80ff1278e97a8bee1f5046e889985b499
RudyMasGoNext <rudy.mas@go-next.be>
2025-12-09 14:00:12 +0100

    Update to PHP 8.5

--- src/DataConverter.php
+++ src/DataConverter.php
@@ -7,12 +7,12 @@ use SimpleXMLElement;
 use stdClass;
 
 /**
- * Class Data Converter (PHP version 8.4)
+ * Class Data Converter (PHP version 8.5)
  *
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024-2025, rudymas.be. (http://www.rudymas.be/)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 2025.02.10.0
+ * @version 2025.12.09.0
  * @package Tigress\DataConverter
  */
 class DataConverter
@@ -30,7 +30,7 @@ class DataConverter
      */
     public static function version(): string
     {
-        return '2025.02.10';
+        return '2025.12.09';
     }
 
     /**
